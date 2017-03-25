d6cee9937ecf95677c36113dcad825c45481b5e0
csnestic <cs.nestico@gmail.com>
2017-03-24 22:04:29 -0400

    Device selection for recording

--- src/main/java/com/mediaProject/App.java
+++ src/main/java/com/mediaProject/App.java
@@ -32,61 +32,64 @@ public class App {
 	 */
 	public static void main( String[] args ) throws IOException, UnsupportedAudioFileException,
 	LineUnavailableException, InterruptedException, NumberFormatException {
+		// Get all audio devices.
 		Info[] mixerInfo = AudioSystem.getMixerInfo();
 		List<Info> playbackInfo = new ArrayList<Info>();
 		List<Info> recordInfo = new ArrayList<Info>();
+
+		// Sort devices by playback and capture types.
+		// TODO: fix VM to test again in linux.
 		for(int i = 0; i < mixerInfo.length; i++) {
 			if(mixerInfo[i].getDescription().contains("Playback")){
 				playbackInfo.add(mixerInfo[i]);
 			} else if(mixerInfo[i].getDescription().contains("Capture")){
 				recordInfo.add(mixerInfo[i]);
 			}
-			//System.out.println(mixerInfo[i].getDescription() + " | " + mixerInfo[i].getName());
 		}
 
-		//		for(int i = 0; i < playbackInfo.size(); i++) {
-		//			System.out.println(playbackInfo.get(i).getDescription() + " | " + playbackInfo.get(i).getName());
-		//		}
-		//		for(int i = 0; i < recordInfo.size(); i++) {
-		//			System.out.println(recordInfo.get(i).getDescription() + " | " + recordInfo.get(i).getName());
-		//		}
 		String fileName = "";
 		if (args.length == 0){
+			// Empty call.
 			System.out.println("Please specify an operation.");
 		} else if (args.length == 1) {
+			// One argument, record should be the only one that works.
 			if(args[0].equals("play")){
 				System.out.println("Please specify a filename you wish to playback.");
 			} else if (args[0].equals("record")) {
-				Record.record(fileName);
+				// One argument record uses default filename and default recording device.
+				Record.record(fileName, recordInfo.get(0));
 			} else {
 				System.out.println("Unknown Operation.");
 			}
 		} else if (args.length == 2) {
+			// Two arguments, record or play using default audio devices.
 			if (args[0].equals("record")) {
-				Record.record(args[1]);
+				Record.record(args[1], recordInfo.get(0));
 			} else if (args[0].equals("play")) {
 				Playback.playback(args[1], null);
 			} else {
 				System.out.println("Unknown Operation.");
 			}
 		} else if (args.length == 3) {
+			// Three arguments, check for record or play commands
+			// and check that the specified device exists.
 			if (args[0].equals("record")) {
 				int record = Integer.parseInt(args[2]);
 				if (record > recordInfo.size() || record < 0) {
 					System.out.println("The requested recording device does not exist.");
 				} else {
-					Record.record(args[1]);
+					Record.record(args[1], recordInfo.get(record));
 				}
-			}
-			if (args[0].equals("play")){
+			} else if (args[0].equals("play")){
 				int playback = Integer.parseInt(args[2]);
 				if (playback > playbackInfo.size() || playback  < 0) {
 					System.out.println("The requested playback device does not exist.");
 				} else {
 					Playback.playback(args[1], playbackInfo.get(playback));
 				}
+			} else {
+				System.out.println("Unknown Operation.");
 			}
-
 		} else {
 			System.out.println("Unknown Operation.");
 		}
--- src/main/java/com/mediaProject/Record.java
+++ src/main/java/com/mediaProject/Record.java
@@ -9,8 +9,9 @@ import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
 import javax.sound.sampled.TargetDataLine;
 
 public class Record {
@@ -50,51 +51,53 @@ public class Record {
 	 *
 	 * @param fileName
 	 */
-	public static void record(String fileName) {
+	public static void record(String fileName, Mixer.Info mixerInfo) {
 		final AudioFormat soundFormat = new AudioFormat(Constants.sampleRate, Constants.sampleSize,
 				Constants.channels, Constants.signed, Constants.bigEndian);
 		// Create the line to begin recording and try/catch for errors.
-		DataLine.Info dataInfo = new DataLine.Info(TargetDataLine.class, soundFormat);
-		try {
-			final TargetDataLine line = (TargetDataLine)AudioSystem.getLine(dataInfo);
-			line.open(soundFormat);
-			line.start();
-			// The audio is recorded into a byte array output stream.
-			Runnable runner = new Runnable() {
-				int bufferSize = (int)soundFormat.getSampleRate()
-						* soundFormat.getFrameSize();
-				byte buffer[] = new byte[bufferSize];
+		Mixer selectedMixer = AudioSystem.getMixer(mixerInfo);
+		Line.Info dataInfo = new Line.Info(TargetDataLine.class);
+		if (selectedMixer.isLineSupported(dataInfo)) {
+			try {
+				final TargetDataLine line = (TargetDataLine) selectedMixer.getLine(dataInfo);
+				line.open(soundFormat);
+				line.start();
+				// The audio is recorded into a byte array output stream.
+				Runnable runner = new Runnable() {
+					int bufferSize = (int)soundFormat.getSampleRate()
+							* soundFormat.getFrameSize();
+					byte buffer[] = new byte[bufferSize];
 
-				@Override
-				public void run() {
-					out = new ByteArrayOutputStream();
-					try {
-						// 10 Second timer for recording.
-						long t = System.currentTimeMillis();
-						long timer = t+10000;
-						System.out.println("Recording (10 seconds)");
-						while (System.currentTimeMillis() < timer) {
-							int counter = line.read(buffer, 0, buffer.length);
-							if (counter > 0){
-								out.write(buffer, 0, counter);
+					@Override
+					public void run() {
+						out = new ByteArrayOutputStream();
+						try {
+							// 10 Second timer for recording.
+							long t = System.currentTimeMillis();
+							long timer = t+10000;
+							System.out.println("Recording (10 seconds)");
+							while (System.currentTimeMillis() < timer) {
+								int counter = line.read(buffer, 0, buffer.length);
+								if (counter > 0){
+									out.write(buffer, 0, counter);
+								}
 							}
+							System.out.println("Recording Complete!");
+							out.close();
+							byte[] tempIs = out.toByteArray();
+							// Send the byte array to the save method.
+							saveCapture(fileName, tempIs, soundFormat);
+
+						}  catch (IOException e) {
+							e.printStackTrace();
 						}
-						System.out.println("Recording Complete!");
-						out.close();
-						byte[] tempIs = out.toByteArray();
-						// Send the byte array to the save method.
-						saveCapture(fileName, tempIs, soundFormat);
-
-					}  catch (IOException e) {
-						e.printStackTrace();
 					}
-				}
-			};
-			Thread recordingThread = new Thread(runner);
-			recordingThread.start();
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
+				};
+				Thread recordingThread = new Thread(runner);
+				recordingThread.start();
+			} catch (LineUnavailableException e) {
+				e.printStackTrace();
+			}
 		}
-
 	}
 }
